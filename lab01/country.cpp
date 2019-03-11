bf440aac8fa884abe9d19518ef2981cbe34cbdf0
Artem Supryhan <suprigan.artem1@gmail.com>
2019-03-11 13:01:18 +0200

    Исправлены функции в соответствии с заданием
    Co-Authored-By: creatortheash <creatortheash@users.noreply.github.com>

--- lab01/country.cpp
+++ lab01/country.cpp
@@ -1,4 +1,5 @@
 #include "file_methods.h"
+#include "country.h"
 
 vector <string> getCountries(int size){
 	vector <string> country;
@@ -8,13 +9,6 @@ vector <string> getCountries(int size){
 	return country;
 }
 
-vector <int> getPoints(int size){
-	vector<int> points;
-	for (int i = 0; i < size; i++){
-		points.push_back(rSum(i));
-	}
-	return points;
-}
 
 void print(vector <string> countries, vector<int> points, int size){
 	for (int i = 0; i < size; i++){
@@ -40,13 +34,59 @@ void sortByScore(vector <string> &countries, vector<int> &points, int size){
 	}
 }
 
-void getFinalScore(vector <int> &points, int size){
-	int score = 12;
-	for (int i = 0; i < 10; i++){
-		points[i] = score;
-		if (i < 2) score -= 2;
-		else score -= 1;
+vector <vector<int>> getVotes(int size){
+	vector <vector <int>> votes;
+	vector <int> temp;
+	for (int i = 0; i < size; i++){
+		for (int j = 0; j < size; j++){
+			temp.push_back(getNum(j, i));
+		}
+		votes.push_back(temp);
+		temp.clear();
+	}
+	return votes;
+}
+
+
+
+void getFinalScore(vector <vector<int>> &votes, int size){
+	for (int i = 0; i < size; i++){
+		int score = 12;
+		for (int count = 1; count <= 10; count++){
+			votes[getMaxIndex(votes, size, i)][i] = score;
+			if (count < 3) score -= 2;
+			else score--;
+		}
+		for (int j = 0; j < size; j++){
+			if (votes[j][i] > 12) votes[j][i] = 0;
+		}
 	}
 }
+
+int getMaxIndex(vector <vector<int>> &votes, int size, int column){
+	int index = 0;
+	int max = votes[0][column];
+	for (int i = 1; i < size; i++){
+		if (max < votes[i][column]){
+			max = votes[i][column];
+			index = i;
+		}
+	}
+	return index;
+}
+
+vector<int> sumRow(vector <vector<int>> votes, int size){
+	int sum = 0;
+	vector<int> sumR;
+	for (int i = 0; i < size; i++){
+		for (int j = 0; j < size; j++){
+			sum += votes[i][j];
+		}
+		sumR.push_back(sum);
+		sum = 0;
+	}
+	return sumR;
+}
+
 
 
