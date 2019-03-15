10552e86ef4bd61be405515a6d9bfe9ebc7bfe08
Artem Supryhan <suprigan.artem1@gmail.com>
2019-03-15 21:18:01 +0200

    Удалены лишние комментарии

--- lab01/main.cpp
+++ lab01/main.cpp
@@ -1,21 +1,6 @@
 #include "file_methods.h"
 #include "country.h"
 
-/*
-void main(){
-	int size = getSize();
-	
-	vector <country> dataBase;
-	getCountries(dataBase, size);
-	getVotesRating(dataBase, size);
-	getScore(dataBase, size);
-	sortByScore(dataBase, size);
-	print(dataBase, size);
-	getResultFile(dataBase, 10);
-	system("pause");
-}
-*/
-
 int main(int argc, char* argv[])
 {
 	vector <int> rate = cmdInput(argc, argv);
